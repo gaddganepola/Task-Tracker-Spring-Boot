d4cd4d1c0f7acad79e92d7219996bdb94c07e3e7
gaddganepola <ganeypola@gmail.com>
2025-08-02 09:06:45 +0530

    create template for task model

---
+++ src/main/java/com/ddg/TaskTracker/model/Task.java
@@ -0,0 +1,15 @@
+package com.ddg.TaskTracker.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@Entity
+public class Task {
+}
